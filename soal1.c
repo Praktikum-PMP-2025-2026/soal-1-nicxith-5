284a32fb6def29ee97ee8a30fef6a71d247ece5e
Saragih, Jan Roman Arthuro <janromansaragih@gmail.com>
2026-05-12 09:02:02 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -13,7 +13,7 @@ int n;
 
 void preorder(int i){
     if (i>=n) return;
-    printf("%d ",tree[i]);
+    printf(" %d",tree[i]);
     preorder(2*i+1);
     preorder(2*i+2);
 }
@@ -21,7 +21,7 @@ void preorder(int i){
 void inorder(int i) {
     if (i>=n) return;
     inorder(2*i+1);
-    printf("%d ", tree[i]);
+    printf(" %d", tree[i]);
     inorder(2*i+2);
 }
 
@@ -29,24 +29,24 @@ void postorder(int i){
     if (i>=n) return;
     postorder(2*i+1);
     postorder(2*i+2);
-    printf("%d ",tree[i]);
+    printf(" %d",tree[i]);
 }
 
 
 int main() {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &tree[i]);  
+        scanf(" %d", &tree[i]);  
     }
-    printf("PRE ");
+    printf("PRE");
     preorder(0);
     printf("\n");
 
-    printf("IN ");
+    printf("IN");
     inorder(0);
     printf("\n");
 
-    printf("POST ");
+    printf("POST");
     postorder(0);
     printf("\n");
 
